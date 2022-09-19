ac10c775e8e15daa27337934a43597abd8b81412
Avi <aqbewtra@gmail.com>
2022-09-19 10:34:24 -0400

    Clean extra comments and unused functions from generator.

--- simple_generator.py
+++ simple_generator.py
@@ -1,12 +1,11 @@
 from torch import nn
 
 class SimpleGenerator(nn.Module):
-    def __init__(self, input_dim, out_features, img_shape, batch_size=None):
+    def __init__(self, input_dim, out_features, img_shape):
         super(SimpleGenerator, self).__init__()
         
         self.input_dim = input_dim
         self.out_features = out_features
-        self.batch_size = batch_size
         self.img_shape = img_shape
 
         self.lin1 = nn.Linear(in_features=self.input_dim, out_features=256)
@@ -16,6 +15,7 @@ class SimpleGenerator(nn.Module):
 
         self.relu = nn.ReLU(inplace=True)
         self.sigmoid = nn.Sigmoid()
+        self.tanh = nn.Tanh()
 
     def forward(self, z):
         z = self.lin1(z)
@@ -26,9 +26,7 @@ class SimpleGenerator(nn.Module):
         z = self.relu(z)
         z = self.lin4(z)
         z = self.sigmoid(z)
-        
-        z = z.view(self.batch_size, *self.img_shape)
-
+        z = z.view(-1, *self.img_shape)
         return z
 
 if __name__ == "__main__":
@@ -39,10 +37,10 @@ if __name__ == "__main__":
     batch_size = 1000
     img_shape = (1, 28, 28)
     z_dim = 100
-    g = SimpleGenerator(input_dim=100, out_features=28*28, img_shape=img_shape, batch_size=batch_size).to(device)
+    g = SimpleGenerator(input_dim=100, out_features=28*28, img_shape=img_shape).to(device)
 
     times = []
-    for i in range(100):
+    for i in range(1):
         start = time.time()
         z = torch.rand((batch_size, z_dim), device=g.lin1.weight.device)
         fake_batch = g(z)
